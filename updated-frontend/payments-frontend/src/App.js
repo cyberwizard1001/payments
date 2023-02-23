c7e631f50264f5a32c1403aba0b790d749be24ac
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2023-02-23 11:34:10 +0530

    updates

--- updated-frontend/payments-frontend/src/App.js
+++ updated-frontend/payments-frontend/src/App.js
@@ -8,28 +8,31 @@ import Vendors from "./pages/Vendors";
 import Billers from "./pages/Billers";
 import Items from "./pages/Items";
 import Orders from "./pages/Orders";
-import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
+      <Navbar />
       <Routes>
           <Route path="/" exact component={Login} />
-          <div>
-        <Navbar />
+      
+
         
           <Route path="/home" element={<Home/>} />
           <Route path="/vendors" component={<Vendors/>} />
           <Route path="/billers" component={<Billers/>} />
           <Route path="/items" component={<Items/>} />
           <Route path="/orders" component={<Orders/>} />
-          <Footer />
-          </div>
           
+          
+            
         </Routes>
         
-      </Router>
+      <Footer />  
+      </BrowserRouter>
+      
     </div>
   );
 }
